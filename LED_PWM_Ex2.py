5a1d52804a1793c48817a0cdad20fae53e2cab08
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2025-01-16 18:25:42 -0500

    Rotation&Acceleration.py

--- LED_PWM_Ex2.py
+++ LED_PWM_Ex2.py
@@ -1,21 +1,65 @@
 #git clone https://github.com/Majdawad88/LED_PWM_Ex2.git
 
 
-import RPi.GPIO as GPIO
+#!/usr/bin/python
+import smbus
+import math
 from time import sleep
-GPIO.setmode(GPIO.BCM)
-LED = 21
-GPIO.setup(LED, GPIO.OUT)
+
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+address = 0x68       # via i2cdetect
+
+# Aktivieren, um das Modul ansprechen zu koennen
+bus.write_byte_data(address, power_mgmt_1, 0)
 
 while True:
-        numBlinks = int(input('Enter Number of Blinks: '))
-        delayOn = int(input('Enter DelayOn: '))
-        delayOff = int(input('Enter DelayOff: '))
 
-        for i in range(0, numBlinks, 1):
-                GPIO.output(LED, GPIO.HIGH)
-                print('LED ON')
-                sleep(delayOn)
-                GPIO.output(LED, GPIO.LOW)
-                print('LED OFF')
-                sleep(delayOff)
+        print ("------------------------------------------------------------------------------")
+
+        beschleunigung_xout = read_word_2c(0x3b)
+        beschleunigung_yout = read_word_2c(0x3d)
+        beschleunigung_zout = read_word_2c(0x3f)
+
+        beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
+        beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
+        beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+
+        print ("beschleunigung_xout: ", ("%6d" % beschleunigung_xout))
+        print ("beschleunigung_yout: ", ("%6d" % beschleunigung_yout))
+        print ("beschleunigung_zout: ", ("%6d" % beschleunigung_zout))
+
+        print ("X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+        print ("Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+
+        sleep(0.5)
